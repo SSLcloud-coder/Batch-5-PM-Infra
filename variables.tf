621ddee59ba444b859c96742630ed0a4b9400e72
ssl-cloud <sslcloudlearning@gmail.com>
2025-11-13 17:36:02 +0530

    rg name changed

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "location" {
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "demo-rg"
+  default     = "demo-rg1"
 }
 
 variable "admin_username" {
